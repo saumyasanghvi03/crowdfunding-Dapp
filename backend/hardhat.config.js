a9ec0a25bde1ac8e74ef7937b22a4561f17c468f
Rahul <prathampathak14@gmail.com>
2023-04-16 12:03:21 +0530

    first commit

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,36 +1,36 @@
-require('@nomicfoundation/hardhat-toolbox');
-require('dotenv').config();
-require('./tasks/task');
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("./tasks/task");
 
-const ACCOUNT1 = process.env.ACCOUNT1;
-const ACCOUNT2 = process.env.ACCOUNT2;
+const ACCOUNT1 = "Paste your private key";
+//const ACCOUNT2 = process.env.ACCOUNT2;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
-  solidity: '0.8.18',
+  solidity: "0.8.18",
   networks: {
     goerli: {
       url: `https://rpc.ankr.com/eth_goerli`,
-      accounts: [ACCOUNT2],
+      accounts: [ACCOUNT1],
     },
     localhost: {
-      url: 'http://localhost:8545',
+      url: "http://localhost:8545",
       chainId: 31337,
     },
     liberty: {
-      url: 'https://liberty20.shardeum.org/',
+      url: "https://liberty20.shardeum.org/",
       chainId: 8081,
       accounts: [ACCOUNT1],
     },
     mumbai: {
-      url: 'https://rpc.ankr.com/polygon_mumbai',
+      url: "https://rpc.ankr.com/polygon_mumbai",
       chainId: 80001,
       accounts: [ACCOUNT1],
     },
     sepolia: {
-      url: 'https://rpc.ankr.com/eth_sepolia',
-      accounts: [ACCOUNT2],
+      url: "https://rpc.ankr.com/eth_sepolia",
+      accounts: [ACCOUNT1],
     },
   },
 };
